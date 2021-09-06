559459b2b7016cec7fee1b2673c8847d0cd980d5
ymzkjpx <ymzkjpx@gmail.com>
2021-09-06 22:04:06 +0900

    first commit

---
+++ src/main/java/com/cloneccsrobjectmapping/CloneCcsrObjectMappingApplication.java
@@ -0,0 +1,13 @@
+package com.cloneccsrobjectmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneCcsrObjectMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneCcsrObjectMappingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloneccsrobjectmapping/CloneCcsrObjectMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloneccsrobjectmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneCcsrObjectMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
