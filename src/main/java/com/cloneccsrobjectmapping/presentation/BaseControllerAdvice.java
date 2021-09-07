0573fba91df660759ec6f64b9d7557db0112954c
ymzkjpx <ymzkjpx@gmail.com>
2021-09-07 14:05:58 +0900

    初期設定を追加

---
+++ src/main/java/com/cloneccsrobjectmapping/presentation/BaseControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.cloneccsrobjectmapping.presentation;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice(basePackageClasses = BaseControllerAdvice.class)
+public class BaseControllerAdvice {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.initDirectFieldAccess();
+        binder.setAllowedFields(""); //どのフィールドも許可しない
+
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
+        binder.registerCustomEditor(Covered.class, new EditorForCovered());
+    }
+}
