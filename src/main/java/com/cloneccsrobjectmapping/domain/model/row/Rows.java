5edcc990a5b62648fcf5181296fe8e5aa5ea3dbb
ymzkjpx <ymzkjpx@gmail.com>
2021-09-08 23:57:56 +0900

    Rowを作成

--- src/main/java/com/cloneccsrobjectmapping/domain/model/row/Rows.java
+++ src/main/java/com/cloneccsrobjectmapping/domain/model/row/Rows.java
@@ -1,19 +1,42 @@
 package com.cloneccsrobjectmapping.domain.model.row;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rows {
-    List<Row> list;
+    @NotNull @Min(value = 1, message = "Between 1~3")
+    @Max(value = 1, message = "Between 1~3")
+    List<@Valid Row> list;
 
     @Deprecated
-    Rows(){}
+    Rows() {
+    }
 
     public Rows(List<Row> list) {
         this.list = list;
     }
 
-    public List<Row> asList(){
+    public List<Row> asList() {
         return list;
+    }
+
+    public Rows addRow() {
+        list.add(new Row());
+        return new Rows(new ArrayList<>(list));
+    }
+
+    public Rows removeRow(int index){
+        list.remove(index);
+        return new Rows(new ArrayList<>(list));
+    }
+
+    private boolean canRemove(int index){
+
     }
 
     @Override
